39b2ff87e0c09c524e84452d06bfbc31aecada71
uimarshall <uimarshall@gmail.com>
2020-11-10 09:30:46 +0100

    Add project configuration and todoItem constructor

--- src/app/app.js
+++ src/app/app.js
@@ -1,19 +1,10 @@
-let doc;
-doc = document;
-doc = document.head;
-doc = document.body;
-doc = document.doctype;
-doc = document.domain;
-doc = document.documentURI;
-doc = document.URL;
-// doc = document.contentType;
-// doc = document.characterSet;
-// doc = document.links;
-// doc = document.links[0].classList;
-// doc = document.links[0].className;
-// doc = document.images;
+let todoItem = (project, title, description, date, completed, priority)=>{
+    this.project = project
+    this.title = title
+    this.description = description
+    this.date = date
+    this.completed = completed
+    this.priority = priority
+}
 
-const para = document.createElement('p');
-para.textContent = `My url is ${doc}`;
-
-document.body.appendChild(para);
+export default todoItem
