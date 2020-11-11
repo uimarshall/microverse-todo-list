9d9ff82f96a8b899e9766a287748a930346e2233
uimarshall <uimarshall@gmail.com>
2020-11-11 16:53:58 +0100

    Add styles to addProject form

--- src/app/addProject.js
+++ src/app/addProject.js
@@ -4,14 +4,16 @@ import contentCreator from "../helpers/contentCreator";
 export default function addProject(){
    const form = document.createElement('form')
 
-   const inputWrapper = contentCreator.withText('div', '', 'form-control')
+   const inputWrapper = contentCreator.withText('div', '', 'form-group')
    const inputField = contentCreator.withoutLabel('input', 'text', 'Project Title', 'todo-input')
+    inputField.classList.add('form-control')
    inputWrapper.appendChild(inputField)
    form.appendChild(inputWrapper)
 
 
-   const submitWrapper = contentCreator.withText('div', '', 'form-control')
+   // const submitWrapper = contentCreator.withText('div', '', 'form-control')
    const submitBtn = contentCreator.withoutLabel('input', 'submit', '', 'compconsted')
+   submitBtn.classList.add('btn', 'btn-info', 'my-3')
    submitBtn.value = "Add Project"
    submitBtn.onclick = (e) => {
       if(localStorage[inputField.value]){
@@ -21,8 +23,9 @@ export default function addProject(){
          localStorage.setItem(`${inputField.value}`, "")
       }
    }
-   submitWrapper.appendChild(submitBtn)
-   form.appendChild(submitWrapper)
+   // submitWrapper.appendChild(submitBtn)
+   // form.appendChild(submitWrapper)
+   form.appendChild(submitBtn)
 
    form.classList.add('default');
 
