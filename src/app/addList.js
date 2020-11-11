7f306481b1aaf65c32b0520d979a42c2f98552cf
uimarshall <uimarshall@gmail.com>
2020-11-11 16:44:01 +0100

    Add styles to form

--- src/app/addList.js
+++ src/app/addList.js
@@ -8,7 +8,7 @@ function addToStorage(project, list){
 export default function addTodos(){
    const form = document.createElement('form')
 
-    const inputWrapper = contentCreator.withText('div', '', 'form-control')
+    const inputWrapper = contentCreator.withText('div', '', 'form-group')
     const projects = []
     for(let i = 0; i < localStorage.length; i += 1){
       if (localStorage.key(i) != 'loglevel:webpack-dev-server'){
@@ -16,30 +16,37 @@ export default function addTodos(){
       }
    }
    const projectSelect = contentCreator.selectMenu(projects)
+   projectSelect.classList.add('form-control')
     inputWrapper.appendChild(projectSelect)
     form.appendChild(inputWrapper)
 
-    const inputWrapper2 = contentCreator.withText('div', '', 'form-control')
+    const inputWrapper2 = contentCreator.withText('div', '', 'form-group')
     const inputField2 = contentCreator.withoutLabel('input', 'text', 'title', 'todo-input')
+    inputField2.classList.add('form-control')
     inputWrapper2.appendChild(inputField2)
     form.appendChild(inputWrapper2)
 
-    const inputWrapper3 = contentCreator.withText('div', '', 'form-control')
+    const inputWrapper3 = contentCreator.withText('div', '', 'form-group')
+    
     const textarea =  contentCreator.withText('textarea', '', 'desc')
+    textarea.classList.add('form-control')
+    textarea.setAttribute('placeholder', 'Enter Description')
     inputWrapper3.appendChild(textarea)
     form.append(inputWrapper3)
 
-    const textBoxWrapper = contentCreator.withText('div', '', 'form-control')
+    const textBoxWrapper = contentCreator.withText('div', '', 'form-check')
     const textBoxField = contentCreator.withLabel('input', 'checkbox', '', 'compconsted', 'completed')
     textBoxWrapper.appendChild(textBoxField)
     form.appendChild(textBoxWrapper)
 
    const menuOptions = ["low", "medium", "high"]
    const selectField = contentCreator.selectMenu(menuOptions)
+   selectField.classList.add('form-control')
    form.appendChild(selectField)
 
-   const submitWrapper = contentCreator.withText('div', '', 'form-control')
+   // const submitWrapper = contentCreator.withText('div', '', 'form-control')
    const submitBtn = contentCreator.withoutLabel('input', 'submit', '', 'compconsted')
+   submitBtn.classList.add('btn', 'btn-info', 'my-3')
    submitBtn.value = "Add Todos"
    submitBtn.onclick = () => {
       let list = {
@@ -50,8 +57,9 @@ export default function addTodos(){
       }
       addToStorage(projectSelect.value, list)
    }
-   submitWrapper.appendChild(submitBtn)
-   form.appendChild(submitWrapper)
+   // submitWrapper.appendChild(submitBtn)
+   
+   form.appendChild(submitBtn)
 
    form.classList.add('default');
 
